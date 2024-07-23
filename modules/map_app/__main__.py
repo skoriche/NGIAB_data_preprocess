ec0818b7a9ac297da7db06d33b3f0a60a33403a2
Josh Cunningham <josh.cu@gmail.com>
2024-07-23 12:06:47 -0500

    fix port 5000 in use (#33)

--- modules/map_app/__main__.py
+++ modules/map_app/__main__.py
@@ -39,7 +39,6 @@ with open("app.log", "w") as f:
     f.write("")
     f.write("Starting Application!\n")
 
-logging.getLogger("werkzeug").setLevel(logging.WARNING)
 
 app = Flask(__name__)
 app.register_blueprint(main)
@@ -63,17 +62,35 @@ logging.getLogger("").addHandler(console_handler)
 
 
 def open_browser():
-    webbrowser.open("http://localhost:5000")
+    # find the last line in the log file that contains the port number
+    # * running on http://0.0.0.0:port_number
+    port_number = None
+    with open("app.log", "r") as f:
+        lines = f.readlines()
+        for line in reversed(lines):
+            if "Running on http" in line:
+                port_number = line.split(":")[-1].strip()
+                break
+    if port_number is not None:
+        webbrowser.open(f"http://localhost:{port_number}")
+    else:
+        print("Could not find the port number in the log file. Please open the url manually.")
+    set_logs_to_warning()
+
+
+def set_logs_to_warning():
+    logging.getLogger("werkzeug").setLevel(logging.WARNING)
 
 
 if __name__ == "__main__":
 
     if file_paths.dev_file().is_file():
+        Timer(2, set_logs_to_warning).start()
         with open("app.log", "a") as f:
             f.write("Running in debug mode\n")
-        app.run(debug=True, host="0.0.0.0", port=5000)
+        app.run(debug=True, host="0.0.0.0", port="0")
     else:
         Timer(1, open_browser).start()
         with open("app.log", "a") as f:
             f.write("Running in production mode\n")
-        app.run(host="0.0.0.0", port=5000)
+        app.run(host="0.0.0.0", port="0")
