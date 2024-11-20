87183db0ff17c5c99163a50bf07d2ae18be526d9
Josh Cunningham <josh.cu@gmail.com>
2024-11-20 15:31:26 -0600

    Add gages to map (#60)
    * update map, add gages
    
    * update map style url

--- modules/map_app/static/js/main.js
+++ modules/map_app/static/js/main.js
@@ -63,3 +63,53 @@ map.on('click', 'catchments', (e) => {
     cat_id = e.features[0].properties.divide_id;    
     update_map(cat_id, e);
 });
+
+// Create a popup, but don't add it to the map yet.
+const popup = new maplibregl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+
+map.on('mouseenter', 'conus_gages', (e) => {
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
+
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const description = e.features[0].properties.hl_uri + "<br> click for more info";
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(coordinates).setHTML(description).addTo(map);
+});
+
+map.on('mouseleave', 'conus_gages', () => {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+});
+
+map.on('click', 'conus_gages', (e) => {
+    //  https://waterdata.usgs.gov/monitoring-location/02465000 
+    window.open("https://waterdata.usgs.gov/monitoring-location/" + e.features[0].properties.hl_link, '_blank');
+}
+);
+show = true;
+const toggleButton = document.querySelector('#toggle-button');
+toggleButton.addEventListener('click', () => {
+    if (show) {
+        map.setFilter('conus_gages', ['any', ['==', 'hl_uri', ""]])
+        toggleButton.innerText = 'Show gages';
+        show = false;
+    } else {
+        map.setFilter('conus_gages', null)
+        toggleButton.innerText = 'Hide gages';
+        show = true;
+    }
+});
+
